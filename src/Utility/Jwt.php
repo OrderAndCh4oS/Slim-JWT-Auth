69df1d9556bec33af13c47e7b7f379b526e4120e
Sarcoma <sarcoma@live.co.uk>
2017-11-24 12:46:32 +0000

    Added env parameters for JWT

--- src/Utility/Jwt.php
+++ src/Utility/Jwt.php
@@ -11,22 +11,18 @@ use Slim\Http\Request;
 
 class Jwt
 {
-    private static $key = '**06-russia-STAY-dollar-95**';
-    private static $issuer = 'http://slim-jwt-auth.dev';
-    private static $audience = 'http://vue-client.dev';
-
     public static function create($username, $roles)
     {
         $signer = new Sha256();
-        $token = (new Builder())->setIssuer(self::$issuer)// Configures the issuer (iss claim)
-        ->setAudience(self::$audience)// Configures the audience (aud claim)
+        $token = (new Builder())->setIssuer(getenv('JWT_ISSUER'))// Configures the issuer (iss claim)
+        ->setAudience(getenv('JWT_AUDIENCE'))// Configures the audience (aud claim)
         ->setId('4f1g23a12aa', true)// Configures the id (jti claim), replicating as a header item
         ->setIssuedAt(time())// Configures the time that the token was issue (iat claim)
         ->setNotBefore(time())// Configures the time that the token can be used (nbf claim)
         ->setExpiration(time() + 3600)// Configures the expiration time of the token (exp claim)
         ->set('username', $username)// Configures a new claim, called "username"
         ->set('roles', $roles)// Configures a new claim, called "roles"
-        ->sign($signer, self::$key)// creates a signature using key
+        ->sign($signer, getenv('JWT_KEY'))// creates a signature using key
         ->getToken(); // Retrieves the generated token
 
         return (string)$token;
@@ -52,10 +48,10 @@ class Jwt
     public static function check(Token $token)
     {
         $signer = new Sha256();
-        if (!$token->verify($signer, self::$key)) {
+        if (!$token->verify($signer, getenv('JWT_KEY'))) {
             throw new \InvalidArgumentException();
         }
 
-        return $token->verify($signer, self::$key);
+        return $token->verify($signer, getenv('JWT_KEY'));
     }
 }
