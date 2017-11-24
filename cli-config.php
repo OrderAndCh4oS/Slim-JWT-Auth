2ebb4136794795fdeb9a9bdb9e20562b03b043d6
Sarcoma <sarcoma@live.co.uk>
2017-11-24 12:19:26 +0000

    Actually used dotenv

--- cli-config.php
+++ cli-config.php
@@ -1,7 +1,11 @@
 <?php
 use Doctrine\ORM\Tools\Console\ConsoleRunner;
+use Dotenv\Dotenv;
 
 require __DIR__ . '/vendor/autoload.php';
+$dotEnv = new Dotenv(__DIR__.'/src');
+$dotEnv->load();
+$dotEnv->required(['DB_HOST', 'DB_NAME', 'DB_USER', 'DB_PASS']);
 
 $settings = require __DIR__ . '/src/settings.php';
 $doctrine = $settings['settings']['doctrine'];
